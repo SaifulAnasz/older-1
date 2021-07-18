4bf116e698e23facb63a2a9e4e23e8d48d920c4d
catpea <corporate.miou@gmail.com>
2021-07-18 13:47:11 -0400

    New Release

---
+++ test.js
@@ -0,0 +1,8 @@
+#!/bin/env node
+
+import {equal, deepEqual} from 'assert';
+import older from './index.js';
+
+const expected = true;
+const actual = older('LICENSE', 'package.json');
+equal(actual, expected);
